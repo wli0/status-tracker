43a27d59c98ca6d473e501ac7f35aad51ede46b5
wli0 <144973371+wli0@users.noreply.github.com>
2026-04-14 13:39:57 -0400

    Remove unused file

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,16 @@ const INITIAL_CLIENTS = [
 ];
 
 function App(){
+  //clients = current client data
+  //setClients = function to update client data
   const [clients, setClients] = useState(INITIAL_CLIENTS);
+
+  //notification = toast message text
+  //setNotification = function to change the toast message
   const [notification, setNotification] = useState("");
 
+  //rhis effect runs whenever notification changes
+  //if a toast message appears, clear it after 3 seconds.
   useEffect(() => {
     if(!notification) return;
 
@@ -30,9 +37,13 @@ function App(){
       setNotification("");
     }, 3000);
 
+    //cleanup if effect runs again before 3 seconds ends,
+    //1remove the old timer first.
     return () => clearTimeout(timer);
   }, [notification]);
 
+  //helper function to send a Discord webhook.
+  //it sends a POST request with the client name and status change.
   const triggerNotification = async (clientName, oldStatus, newStatus) => {
     try{
       await fetch("https://discord.com/api/webhooks/1493079642796720138/xcaxNG5rIGo29gNZsKf65Ywej4SNwB1q8_jn-AoFwGIaejS6_ThJKqads80F_grT4WmD", {
@@ -50,15 +61,22 @@ function App(){
     }
   };
 
+  //runs when admin clicks update status
   const handleUpdate = (clientId, newStatus) => {
+    //find the matching client ID 
     const currentClient = clients.find((client) => client.id === clientId);
+    //if not found, stop
     if(!currentClient) return;
 
+    //if status didn't change, don't update anything, just show a message.
     if(currentClient.status === newStatus) {
       setNotification(`${currentClient.name} is already ${newStatus}.`);
       return;
     }
 
+    //create a new updated clients array
+    //if the client matches, update its status and updatedAt time
+    //otherwise keep it the same
     const updatedClients = clients.map((client) =>
       client.id === clientId
         ? {
